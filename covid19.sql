ff13e0c33df8f15c6c7182d315789825abb7c8e0
Adham Eltantawi <adhameltantawi@gmail.com>
2026-03-16 17:34:20 +0200

    use CTE

--- covid19.sql
+++ covid19.sql
@@ -140,6 +140,28 @@ ORDER BY 1,2,3
 
 
 
+-- Calculate cumulative vaccinations per location using CTE and window function
+
+WITH PopvsVac AS (
+SELECT 
+  dea.continent,
+  dea.location,
+  dea.date,
+  dea.population_density,
+  vac.new_vaccinations,
+  SUM(CAST(vac.new_vaccinations AS INT64)) 
+  OVER (PARTITION BY dea.location ORDER BY dea.date) 
+  AS RollingPeopleVaccinated
+FROM `coivid19.CovidDeaths` dea
+JOIN `coivid19.CovidVaccination` vac
+  ON dea.location = vac.location
+  AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+)
+
+SELECT *, (RollingPeopleVaccinated/population_density)*100
+FROM PopvsVac
+ORDER BY location, date;
 
 
 
